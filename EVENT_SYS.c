5d2b401cb871e1d88cc271f36f007d5362d6da48
esraashawky111 <125348768+esraashawky111@users.noreply.github.com>
2024-10-14 12:42:43 +0300

    Add files via upload

---
+++ EVENT_SYS.c
@@ -0,0 +1,182 @@
+﻿
+/**********************************************************************************************************************
+ *  FILE DESCRIPTION
+/*
+ * EVENT_SYS.c
+ *
+ * Created: 03/10/2024 05:12:13 
+ *  Author: Esraa Ali
+ * Version :VO
+ *
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  INCLUDES
+ *********************************************************************************************************************/
+#include "EVENT_SYS.h"
+
+/**********************************************************************************************************************
+*  LOCAL MACROS CONSTANT\FUNCTION
+*********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  LOCAL DATA 
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  LOCAL FUNCTION PROTOTYPES
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  LOCAL FUNCTIONS
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTIONS
+ *********************************************************************************************************************/
+
+
+/******************************************************************************
+* \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)        
+* \Description     : Describe this service                                    
+*                                                                             
+* \Sync\Async      : Synchronous                                               
+* \Reentrancy      : Non Reentrant                                             
+* \Parameters (in) : parameterName   Parameter Describtion                     
+* \Parameters (out): None                                                      
+* \Return value:   : Std_ReturnType  E_OK
+*                                    E_NOT_OK                                  
+*******************************************************************************/
+ uint8_t EVEN_Set_EVEN[3] = {0};
+	
+static void Event_Push_Elem(uint8_t index) 
+	{
+		 EVEN_Set_EVEN[index]++;
+	}
+	
+static void Event_Pop_Elem(uint8_t index)
+ {
+	if(EVEN_Set_EVEN[index]!=0)
+	{
+		
+	EVEN_Set_EVEN[index]--;
+	
+	}
+}
+/******************************************************************************
+* \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+* \Description     : Describe this service
+*
+* \Sync\Async      : Synchronous
+* \Reentrancy      : Non Reentrant
+* \Parameters (in) : parameterName   Parameter Describtion
+* \Parameters (out): None
+* \Return value:   : Std_ReturnType  E_OK
+*                                    E_NOT_OK
+*******************************************************************************/
+void Buttons_Pressed_Check(){
+  static uint8_t flag0_pre=0, flag1_pre=0, flag2_pre=0;
+  static uint8_t flag2_current = 0, flag1_current = 0, flag0_current = 0;
+	  flag0_current = BTN_GetValue(PORTD_PIN0);
+	  flag1_current = BTN_GetValue(PORTD_PIN1);
+	  flag2_current = BTN_GetValue(PORTD_PIN2);
+	if(flag0_current == 0 && flag0_pre == 1 ){
+		//LCD_WriteChar('F');
+		Event_Push_Elem(EVENT_SW0_PRESSED);
+		flag0_pre = flag0_current;
+	}
+	else if(flag0_current == 1)
+	{
+		
+		flag0_pre = 1;
+		
+	}
+	else
+	 {
+		
+	}
+	
+	
+	if(flag1_current == 0 && flag1_pre == 1 )
+	{
+		//LCD_WriteChar('g');
+		Event_Push_Elem(EVENT_SW1_PRESSED);
+		flag1_pre = flag1_current;
+	}
+	else if(flag1_current == 1)
+	{
+		
+		flag1_pre = 1;
+		
+	}
+	else 
+	{
+		
+	}
+	
+
+	if(flag2_current == 0 && flag2_pre == 1 )
+	{
+		//LCD_WriteChar('h');
+		Event_Push_Elem(EVENT_SW2_PRESSED);
+		flag2_pre = flag2_current;
+	}
+	else if(flag2_current == 1)
+	{
+		
+		flag2_pre = 1;
+		
+	}
+	else 
+	{
+		
+	}
+	
+}
+
+/******************************************************************************
+* \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+* \Description     : Describe this service
+*
+* \Sync\Async      : Synchronous
+* \Reentrancy      : Non Reentrant
+* \Parameters (in) : parameterName   Parameter Describtion
+* \Parameters (out): None
+* \Return value:   : Std_ReturnType  E_OK
+*                                    E_NOT_OK
+*******************************************************************************/
+void APP_Handle_Event(void){
+	   //uint8_t E = 0;
+
+		while(EVEN_Set_EVEN[EVENT_SW0_PRESSED] != 0)
+		{
+		//LCD_WriteChar('A');
+		LED_Toggle(PORTC_PIN0);
+		Event_Pop_Elem(EVENT_SW0_PRESSED);
+		}
+		
+		while(EVEN_Set_EVEN[EVENT_SW1_PRESSED] != 0)
+		{
+	    // LCD_WriteChar('B');
+		LED_Toggle(PORTC_PIN1);
+		Event_Pop_Elem(EVENT_SW1_PRESSED);
+		}
+		
+		while(EVEN_Set_EVEN[EVENT_SW2_PRESSED] != 0)
+		{
+		//LCD_WriteChar('C');
+		LED_Toggle(PORTC_PIN2);
+		Event_Pop_Elem(EVENT_SW2_PRESSED);
+		}
+		
+		
+	
+}
+
+/**********************************************************************************************************************
+ *  END OF FILE: FileName.c
+ *********************************************************************************************************************/
---
+++ main.c
@@ -0,0 +1,163 @@
+/*
+ * 
+ *
+ * Created: 8/12/2023 3:34:43 PM
+ * Author : Esraa Ali
+ */ 
+#define  F_CPU  16000000U
+#include <util/delay.h>
+#include "LED.h"
+#include "BTN.h"
+#include "PORT_Core.h"
+#include "IRQH_Core.h"
+#include "ADC_Core.h"
+#include "LCD_Core.h"
+#include "GPT_Core.h"
+#include "PWM_Core.h"
+#include "Ultrasonic.h"
+#include "UART_Core.h"
+#include "EEPROM_Core.h"
+//#include <math.h>
+#include "FXP.h"
+#include "LUT.h"
+#include "EVENT_SYS.h"
+
+void Temp_Func(void)
+{
+	static uint8_t CNT = 0;
+	if(CNT == 2){
+	LCD_WriteChar('A');
+	LED_Toggle(PORTC_PIN2);
+	CNT=0;
+   }
+	CNT++;
+}
+
+void main(){
+	
+	
+	PORT_Init();
+	LCD_Init();
+	GPT_Init();
+    IRQH_Set_CallBack( Timer_Counter1_Overflow_VECTOR_INDEX,Buttons_Pressed_Check);
+	IRQH_SetGlobalINT(INT_ENABLE);
+	
+	while(1){
+	
+	 APP_Handle_Event();
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//static uint8_t EXE_TIM_FLOAT = 0;
+//static uint8_t EXE_TIM_FIXED= 0;
+/*
+int main() 
+{
+	PORT_Init();
+	LCD_Init();
+	//GPT_Init();
+	float a =3.31, b = 1.1,result = 0;
+	//float c =3.31, d = 1.1,res=0;
+	//while (1)
+	//{
+     LCD_WriteString("FXP");
+     LCD_WriteString(" ");
+   // DIO_WriteChennel(PORTC_PIN1,PIN_LOW);
+    uint32_t ff = fixed_add(a,b);
+   //  TCNT0 = 0;
+	  LCD_WriteInteger(a*100);
+	  LCD_WriteString(" ");
+	  LCD_WriteInteger(b*100);
+      LCD_GoTo(1,0);
+	
+   // for (int i =0;i<1000;i++)
+	//{
+	
+	 result=fixed_to_float(ff);
+	// }
+	// EXE_TIM_FIXED=TCNT0;
+	// TCNT0=0;
+	  LCD_WriteInteger(result*100);
+	  LCD_WriteString(" ");
+	//  CNT=TCNT0;
+	/// LCD_WriteChar(CNT);
+	//   DIO_WriteChennel(PORTC_PIN1,PIN_HIGH);
+	 //  for (int i =0;i<1000;i++)
+	   //{
+	//	   result= a*b;
+	 //  }
+	 //  EXE_TIM_FLOAT =TCNT0;
+	  
+	  // LCD_WriteInteger(EXE_TIM_FIXED);
+	  //  LCD_WriteString(" ");
+		//LCD_WriteInteger(EXE_TIM_FLOAT);*/
+	//}
+	 
+/*
+     ff = fixed_sub(a,b);
+       result=fixed_to_float(ff);
+      LCD_WriteInteger(result*100);
+      LCD_WriteString(" ");
+	  
+	   ff = fixed_mult(a,b);
+	   result=fixed_to_float(ff);
+	  LCD_WriteInteger(result*100);
+	  LCD_WriteString(" ");
+	  
+	 ff = fixed_DIV(a,b);
+	 result=fixed_to_float(ff);
+	LCD_WriteInteger(result*100);
+	LCD_WriteString(" ");
+	
+	return 0;
+}*/
+
+/*int main() {
+	PORT_Init();
+	LCD_Init();
+//  Sine_with_lut();
+   Sine_without_lut();
+	return 0;
+}*/
+
+//LCD_WriteInteger((b*100));
+// 	// Perform fixed-point addition
+//	uint32_t fixed_sum = fixed_add(a,b);
+
+//
+// 	// Perform fixed-point Subtraction
+// 	uint32_t fixed_subt= fixed_sub(a,b);
+//
+// 	// Perform fixed-point multiplication
+// 	uint32_t fixed_product = fixed_mult(a,b);
+// 	// Perform fixed-point division
+//     uint32_t fixed_div = fixed_DIV(a,b);
+//     LCD_WriteChar(fixed_sum);
+// 	LCD_WriteInteger(fixed_sum);
+//     LCD_WriteString("/");
+// 	LCD_WriteInteger(fixed_subt);
+// 	LCD_WriteString("/");
+// 	LCD_WriteInteger(fixed_product);
+// 	LCD_WriteString("/");
+// 	//LCD_WriteInteger(100);
+// 	LCD_WriteChar(fixed_div);
+// 	LCD_WriteString("/");
