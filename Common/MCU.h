248f55fef914c0d2b8c752402eef86060fe92c1c
esraashawky111 <125348768+esraashawky111@users.noreply.github.com>
2024-10-14 01:59:27 -0800

    Update MCU.h

--- Common/MCU.h
+++ Common/MCU.h
@@ -57,24 +57,138 @@
 #define ADC_INPUT_16BIT_ACCESS    (*(volatile uint16_t*)(0x24))
 #define SFIOR                      (*(volatile uint8_t *)(0x50))
 /**************************************** Timers *****************************************************/
+typedef union {
+	struct
+	{
+		uint8_t CS00 :1;
+		uint8_t CS01 :1;
+		uint8_t CS02 :1;
+		uint8_t WGM01:1;
+		uint8_t COM00:1;
+		uint8_t COM01 :1;
+		uint8_t WGM00 :1;
+		uint8_t FOC0 :1;
+	};
+	struct{
+		uint8_t CS:3 ;
+		uint8_t    :1;
+		uint8_t COM:2;
+		uint8_t   :1;
+		uint8_t    :1;
+	};
+}TCCR0_t;
+
+#define TCCR0_bits (*(volatile TCCR0_t*)(0x53))
 #define TCNT0               (*(volatile uint8_t*)(0x52))
 #define TCCR0               (*(volatile uint8_t*)(0x53))
 #define OCR0                (*(volatile uint8_t*)(0x5C))
+typedef struct
+{
+	uint8_t _TOIE0 :1;
+	uint8_t OCIE0 :1;
+	uint8_t TOIE1 :1;
+	uint8_t OCIE1B:1;
+	uint8_t OCIE1A:1;
+	uint8_t _TICIE1:1;
+	uint8_t TOIE2 :1;
+	uint8_t OCIE2 :1;
+}TIMSK_bits_t;
+#define TIMSK_bits (*(volatile TIMSK_bits_t*)(0x59))
 #define TIMSK               (*(volatile uint8_t*)(0x59))
+typedef struct
+{
+	uint8_t TOV0 :1;
+	uint8_t OCF0 :1;
+	uint8_t TOV1 :1;
+	uint8_t OCF1B:1;
+	uint8_t OCF1A:1;
+	uint8_t ICF1 :1;
+	uint8_t TOV2 :1;
+	uint8_t OCF2 :1;
+}TIFR_bits_t;
+#define TIFR_bits (*(volatile TIFR_bits_t*)(0x58))
+#define TIFR (*(volatile uint8_t*)(0x58))
+
 #define TCCR1A              (*(volatile uint8_t* )(0x4F))
+
+#define TCCR1_bits (*(volatile TCCR1_t*)(0x4E))
 #define TCCR1B              (*(volatile uint8_t* )(0x4E))
 #define TCNT1H              (*(volatile uint8_t* )(0x4D))
 #define TCNT1L              (*(volatile uint8_t* )(0x4C))
 #define TCNT1_16BIT_ACCESS  (*(volatile uint16_t*)(0x4C))
+
+
+typedef union {
+	struct
+	{
+		uint8_t CS10 :1;
+		uint8_t CS11 :1;
+		uint8_t CS12 :1;
+		uint8_t WGM12:1;
+		uint8_t WGM13:1;
+		uint8_t      :1;
+		uint8_t _ICES1 :1;
+		uint8_t ICNC1 :1;
+		uint8_t WGM10 :1;
+		uint8_t WGM11 :1;
+		uint8_t FOC1B :1;
+		uint8_t FOC1A:1;
+		uint8_t COM1B0:1;
+		uint8_t COM1B1 :1;
+		uint8_t COM1A0 :1;
+		uint8_t COM1A1 :1;
+	};
+	struct{
+		uint8_t CS :3;
+		uint8_t :1;
+		uint8_t :1;
+		uint8_t      :1;
+		uint8_t :1;
+		uint8_t :1;
+		uint8_t  :1;
+		uint8_t :1;
+		uint8_t :1;
+		uint8_t :1;
+		uint8_t COM1B:2;
+		uint8_t COM1A :2;
+	};
+	
+}TCCR1_t;
+
 #define OCR1AH              (*(volatile uint8_t*)(0x4B))
 #define OCR1AL              (*(volatile uint8_t*)(0x4A))
 #define OCR1A_16BIT_ACCESS  (*(volatile uint16_t*)(0x4A))
+ typedef union {
+	struct
+	{
+		uint8_t _CS20 :1;
+		uint8_t _CS21 :1;
+		uint8_t _CS22 :1;
+		uint8_t _WGM21:1;
+		uint8_t _COM20:1;
+		uint8_t _COM21 :1;
+		uint8_t _WGM20 :1;
+		uint8_t _FOC2 :1;
+	};
+	struct{
+		uint8_t _CS2:3 ;
+		uint8_t    :1;
+		uint8_t _COM2:2;
+		uint8_t    :1;
+		uint8_t    :1;
+	};
+}TCCR2_t;
+
+#define TCCR2_bits (*(volatile TCCR2_t*)(0x45))
+#define TCCR2 (*(volatile uint8_t*)(0x45))
 #define OCR1BH              (*(volatile uint8_t*)(0x49))
 #define OCR1BL              (*(volatile uint8_t*)(0x48))
 #define OCR1B_16BIT_ACCESS  (*(volatile uint16_t*)(0x48))
 #define ICR1H               (*(volatile uint8_t*)(0x47))
 #define ICR1L               (*(volatile uint8_t*)(0x46))
 #define ICR1_16BIT_ACCESS   (*(volatile uint16_t*)(0x46))
+#define TCNT2 (*(volatile uint8_t*)(0x44))
+#define TCNT1 (*(volatile uint16_t*)(0x4C))
 
 /************************************  UART  *****************************************************************/
 
@@ -128,5 +242,6 @@ typedef struct
 /**********************************************************************************************************************
  *  END OF FILE: 
  *********************************************************************************************************************/
+
 
 
